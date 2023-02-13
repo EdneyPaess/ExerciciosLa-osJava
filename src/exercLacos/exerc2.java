bcd3ce8faeabc2a2163b583e12124b8ec05c747c
Edney Paes <Edney19982010@gmail.com>
2023-02-13 14:58:13 -0300

    atualização exercicio 2

--- src/exercLacos/exerc2.java
+++ src/exercLacos/exerc2.java
@@ -1,17 +1,29 @@
 package exercLacos;
 
+import java.util.Scanner;
+
 public class exerc2 {
 
 	public static void main(String[] args) {
-		
+
+		int par=0, impar=0;
+		Scanner entrada= new Scanner(System.in);
 		
 		for (int numero = 1; numero <= 10; numero++) {
+			System.out.println("Digite um numero: ");
+			numero= entrada.nextInt();
+			
 			if(numero % 2 == 0) {
-				System.out.println("\n Numero par: "+numero);
+				par++;
+				
 			}else if(numero % 2 ==1) {
-				System.out.println("\n Numero impar: "+numero);
+				impar++;
+				
 			}
+			
 		}
+		System.out.println("\n Impar: "+impar);
+		System.out.println("\n Par: "+par);
 	}
 }
 
